8a0d3334f35547952b5526c2e9a47bc67c476d0a
gustso <gustso@gmail.com>
2025-01-09 20:51:51 -0300

    segundo commit con un componente nuevo

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+
+function Home(){
+
+    return(
+        <>
+        <h1>Hola Mundo</h1>
+        <img src="https://media.istockphoto.com/id/2122148349/es/foto/hacer-un-examen-en-la-universidad.webp?b=1&s=612x612&w=0&k=20&c=cCbN4LYic90GVVF8n3e5eEWnNKxBq4ow_PTUNAZ4z3c=" alt="imagen de prueba"></img>        
+        <h2>pie de paginas</h2>
+        </>
+    )
+
+
+}
+
+export default Home
